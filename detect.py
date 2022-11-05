67772aee534812488e6d1bbdfa3746f1f7004482
Thu Nguyen <22362633+shortthu@users.noreply.github.com>
2022-11-05 21:13:13 +0700

    change model

--- detect.py
+++ detect.py
@@ -4,7 +4,7 @@ import torch
 
 class YoloV5_Detect:
     def __init__(self):
-        self.model_detect = torch.hub.load('ultralytics/yolov5', 'yolov5l6')
+        self.model_detect = torch.hub.load('ultralytics/yolov5', 'yolov5s')
         self.objects = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
     def predict(self,img):
