1a9b14d41b8bd24def0cb8846e117a308023d68e
Thu Nguyen <22362633+shortthu@users.noreply.github.com>
2022-12-16 20:56:00 +0700

    line break

--- detect.py
+++ detect.py
@@ -1,11 +1,27 @@
 import cv2
 import matplotlib.pyplot as plt
 import torch
+import random
 
 class YoloV5_Detect:
     def __init__(self):
         self.model_detect = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-        self.objects = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+        self.objects = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 
+        'bus', 'train', 'truck', 'boat', 'traffic light', 
+        'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 
+        'cat', 'dog', 'horse', 'sheep', 'cow', 
+        'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 
+        'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 
+        'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 
+        'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 
+        'wine glass', 'cup', 'fork', 'knife', 'spoon', 
+        'bowl', 'banana', 'apple', 'sandwich', 'orange', 
+        'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 
+        'cake', 'chair', 'couch', 'potted plant', 'bed', 
+        'dining table', 'toilet', 'tv', 'laptop', 'mouse', 
+        'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 
+        'toaster', 'sink', 'refrigerator', 'book', 'clock', 
+        'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
     def predict(self,img):
         # img = cv2.imread(image_src)
